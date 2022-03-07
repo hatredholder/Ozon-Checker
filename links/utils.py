9f566ced6f9b68d9f5b81edc0ce73883de32f645
hatredholder <the.great.justice@bk.ru>
2022-03-07 13:59:35 +0300

    removed unnecessary imports

--- links/utils.py
+++ links/utils.py
@@ -1,7 +1,5 @@
-import time
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from fake_useragent import UserAgent
 
@@ -18,8 +16,6 @@ def get_info(url):
 
     try:
         driver.get(url)
-        with open("file.html", "w", encoding='utf-8') as file:
-            file.write(driver.page_source)
         soup = BeautifulSoup(driver.page_source, "lxml")
         name = soup.find(attrs={"data-widget":"webProductHeading"}).next_element.text.strip()
         price = int("".join([i for i in soup.find(attrs={"slot":"content"}).next_element.next_sibling.next_element.next_element.next_element.text if i.isnumeric()]))
