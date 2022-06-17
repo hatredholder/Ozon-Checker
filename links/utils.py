53a37e216e0ce6bc709114f60034dd74a55a4b91
hatredholder <the.great.justice@bk.ru>
2022-06-17 16:10:13 +0300

    commented utils.py

--- links/utils.py
+++ links/utils.py
@@ -5,7 +5,9 @@ from fake_useragent import UserAgent
 
 
 def get_info(product_name):
-    """Get info from URL using Selenium"""
+    """Get info from the API of requested item"""
+
+    # Create a fake user agent
     useragent = UserAgent()
 
     headers = {
@@ -14,19 +16,19 @@ def get_info(product_name):
         'Content-Type': 'application/json',
     }
 
-    try:
-        r = requests.get(f"https://www.ozon.ru/api/composer-api.bx/page/json/v2?url=%2Fproduct%2F{product_name}", headers=headers)
-        
-        name = json.loads(r.json().get("widgetStates").get("webCharacteristics-545710-default-1")).get("productTitle")
+    # Connect to the API
+    r = requests.get(f"https://www.ozon.ru/api/composer-api.bx/page/json/v2?url=%2Fproduct%2F{product_name}", headers=headers)
+    
+    # Get the name
+    name = json.loads(r.json().get("widgetStates").get("webCharacteristics-545710-default-1")).get("productTitle")
 
-        result = []
-        for i in json.loads(r.json().get("widgetStates").get("webPrice-952422-default-1")).get("price"):
-            if i.isnumeric():
-                result.append(i)
-        price = int("".join(result))
+    # Get the price, 
+    # turn the price string to a number
+    result = []
+    for i in json.loads(r.json().get("widgetStates").get("webPrice-952422-default-1")).get("price"):
+        if i.isnumeric():
+            result.append(i)
+    price = int("".join(result))
 
-        return name, price
-
-    except Exception as e:
-        print(e)
+    return name, price
 
