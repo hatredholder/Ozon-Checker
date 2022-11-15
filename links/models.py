bba396053118e0494d2b4a31e03d4771fa49b735
hatredholder <rattlingserpent@gmail.com>
2022-11-15 23:44:15 +0300

    formatted code

--- links/models.py
+++ links/models.py
@@ -3,7 +3,6 @@ from django.db import models
 
 class Link(models.Model):
     """URL Link Info Model"""
-
     name = models.CharField(max_length=220, blank=True)
     url = models.URLField()
     current_price = models.FloatField(default=0)
